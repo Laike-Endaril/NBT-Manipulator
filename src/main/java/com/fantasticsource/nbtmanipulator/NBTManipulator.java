c979a1e2c5cf8024b25a389c80c3f14bebbe301d
Kyle Koder <silvertallon@gmail.com>
2019-08-27 22:27:37 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.nbtmanipulator;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION)
+public class NBTManipulator
+{
+    public static final String MODID = "nbtmanipulator";
+    public static final String NAME = "NBT Manipulator";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(NBTManipulator.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
