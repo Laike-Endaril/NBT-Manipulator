9838ef378b4688b8e5d3a53a86510b130cbe6c17
Kyle Koder <silvertallon@gmail.com>
2019-09-04 22:33:12 -0700

    Many improvements to library.  Added save and cancel buttons (no functionality yet)

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTGUI.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTGUI.java
@@ -3,6 +3,7 @@ package com.fantasticsource.nbtmanipulator;
 import com.fantasticsource.mctools.MCTools;
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.guielements.rect.GUIGradientRect;
+import com.fantasticsource.mctools.gui.guielements.rect.text.GUITextButton;
 import com.fantasticsource.mctools.gui.guielements.rect.text.MultilineTextInput;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
@@ -18,6 +19,9 @@ public class NBTGUI extends GUIScreen
     public static final NBTGUI GUI = new NBTGUI();
 
     private static ArrayList<String> lines;
+    private static GUITextButton saveButton, cancelButton;
+    private static MultilineTextInput code;
+
 
     public static void show(int mode, String nbtString)
     {
@@ -32,9 +36,27 @@ public class NBTGUI extends GUIScreen
         //Background
         guiElements.add(new GUIGradientRect(this, 0, 0, 1, 1, BLACK, BLACK, BLACK, BLACK));
 
+
+        //Buttons
+        saveButton = new GUITextButton(this, 0, 0, "Save", Color.GREEN);
+        guiElements.add(saveButton);
+        cancelButton = new GUITextButton(this, saveButton.x + saveButton.width, 0, "Cancel", Color.RED);
+        guiElements.add(cancelButton);
+
+
         //Multiline Text Input
-        MultilineTextInput multi = new MultilineTextInput(this, 0, 0, 1, 1, lines.toArray(new String[0]));
-        guiElements.add(multi);
-        multi.get(0).setActive(true);
+        double y = saveButton.height;
+        code = new MultilineTextInput(this, 0, y, 1, 1 - y, lines.toArray(new String[0]));
+        guiElements.add(code);
+        code.get(0).setActive(true);
+    }
+
+    @Override
+    public void onResize(Minecraft mcIn, int w, int h)
+    {
+        super.onResize(mcIn, w, h);
+        cancelButton.x = saveButton.x + saveButton.width;
+        code.y = saveButton.y + saveButton.height;
+        code.height = 1 - code.y;
     }
 }
