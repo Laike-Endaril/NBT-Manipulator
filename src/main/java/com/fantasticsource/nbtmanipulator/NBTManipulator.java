4eb6e8c3d1bab370569833a3c04a374ca34d9016
Kyle Koder <silvertallon@gmail.com>
2019-09-24 11:13:38 -0700

    1.12.2.002; allows any remote version, so that non-ops can play without the mod on their client

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021i,)", acceptableRemoteVersions = "[1.12.2.001," + NBTManipulator.VERSION + "]")
+@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021i,)", acceptableRemoteVersions = "*")
 public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.001a";
+    public static final String VERSION = "1.12.2.002";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
