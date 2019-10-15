30144d354e7b2ec5afc4cfe5dc13b6c371589c46
Kyle Koder <silvertallon@gmail.com>
2019-10-14 18:14:44 -0700

    1.12.2.002c; update to newest lib

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.022,)", acceptableRemoteVersions = "*")
+@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.022a,)", acceptableRemoteVersions = "*")
 public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.002b";
+    public static final String VERSION = "1.12.2.002c";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
