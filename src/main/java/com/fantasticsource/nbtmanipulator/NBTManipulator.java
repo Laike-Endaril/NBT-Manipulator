2b37393b162e28164d63ba826b63fdc291668002
Kyle Koder <silvertallon@gmail.com>
2019-11-30 13:01:43 -0800

    Update lib, req, and version for better end brace handling

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.026b,)", acceptableRemoteVersions = "*")
+@Mod(modid = NBTManipulator.MODID, name = NBTManipulator.NAME, version = NBTManipulator.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.027a,)", acceptableRemoteVersions = "*")
 public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.002d";
+    public static final String VERSION = "1.12.2.002e";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
