b3bc5fb9ffe4ad4fea9abf752663e38a705b60e5
Kyle Koder <silvertallon@gmail.com>
2020-08-27 15:02:01 -0700

    1.12.2.004

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -14,7 +14,7 @@ public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.003a";
+    public static final String VERSION = "1.12.2.004";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
