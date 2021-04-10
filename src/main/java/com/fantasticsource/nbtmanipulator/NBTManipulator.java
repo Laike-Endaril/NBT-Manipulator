af1cb0ffa455011265fe96430054e3f4d86c1ac3
Kyle Koder <silvertallon@gmail.com>
2021-04-10 11:33:53 -0700

    1.12.2.004c

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -34,7 +34,7 @@ public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.004b";
+    public static final String VERSION = "1.12.2.004c";
 
     protected static final HashMap<EntityPlayerMP, NBTEditingData> EDITING_TARGETS = new HashMap<>();
 
