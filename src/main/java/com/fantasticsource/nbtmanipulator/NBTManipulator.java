ed0df150ff89c076fdfe86c6902a49e6e24c7286
Kyle Koder <silvertallon@gmail.com>
2021-04-21 18:01:02 -0700

    1.12.2.004h

--- src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
+++ src/main/java/com/fantasticsource/nbtmanipulator/NBTManipulator.java
@@ -36,7 +36,7 @@ public class NBTManipulator
 {
     public static final String MODID = "nbtmanipulator";
     public static final String NAME = "NBT Manipulator";
-    public static final String VERSION = "1.12.2.004g";
+    public static final String VERSION = "1.12.2.004h";
 
     protected static final HashMap<UUID, NBTEditingData> EDITING_TARGETS = new HashMap<>();
 
